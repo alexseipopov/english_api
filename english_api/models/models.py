e1c288b3e521b550e8c498fcec48850a2136f134
Garrosh Magrathean <garroshm@student.21-school.ru>
2023-03-14 20:09:30 +0300

    add status testing fields

--- english_api/models/models.py
+++ english_api/models/models.py
@@ -1,9 +1,10 @@
 import sqlalchemy as sa
 
-from .. import app, db
+from .. import db
 
 
 class User(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
     phone = sa.Column(sa.Text)
     email = sa.Column(sa.Text)
+    status = sa.Column(sa.Text)
