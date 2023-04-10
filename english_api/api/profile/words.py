a869dc26d2dfcf4986d554faa226e47ae7f57426
Alexsei Popov <popov2895@gmail.com>
2023-04-10 07:48:22 +0300

    save stage

--- english_api/api/profile/words.py
+++ english_api/api/profile/words.py
@@ -2,7 +2,7 @@ from flask import request
 from english_api.api import api
 from english_api.utils import auth_check, create_res_obj
 
-@api.post("/word/<id:int>")
+@api.post("/word/<int:id>")
 def get_word():
     if not auth_check(request):
         return create_res_obj(status="FAILURE", description="Not found auth_token in headers"), 400
--- english_api/models/models.py
+++ english_api/models/models.py
@@ -4,11 +4,22 @@ import sqlalchemy as sa
 from .. import db
 
 
+# user_has_word_table = db.Table("user_has_word",
+#     sa.Column("id", sa.BigInteger, primary_key=True),
+#     sa.Column("user_id", sa.Integer, sa.ForeignKey('user.id', ondelete="CASCADE")),
+#     sa.Column("word_id", sa.Integer, sa.ForeignKey('word.id', ondelete="CASCADE")),
+#     sa.Column("status", sa.Integer, sa.ForeignKey('status.id', ondelete="CASCADE"), default=1),
+#     sa.Column("update_time", sa.DateTime, default=datetime.utcnow)
+# )
+
+
 class User(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
     phone = sa.Column(sa.Text, unique=True)
     email = sa.Column(sa.Text, unique=True)
     password = sa.Column(sa.Text)
+    # words = db.relationship('Word', secondary=user_has_word_table, backref='user')
+
 
 
 class Group(db.Model):
@@ -29,6 +40,7 @@ class Word(db.Model):
     image_path = sa.Column(sa.Text)
     group_id = sa.Column(sa.Integer, sa.ForeignKey('group.id', ondelete="CASCADE"), nullable=False)
     group = db.relationship('Group', backref='group', lazy=True)
+    # user = db.relationship('User', secondary=user_has_word_table, backref='word')
 
 
 class Status(db.Model):
@@ -37,12 +49,17 @@ class Status(db.Model):
     description = sa.Column(sa.Text, nullable=False)
 
 
-class User_Has_Word(db.Model):
-    id = sa.Column(sa.BigInteger, primary_key=True)
-    user_id = sa.Column(sa.Integer, sa.ForeignKey("user.id", ondelete='CASCADE'), nullable=False)
-    user = db.relationship("User", backref='user', lazy=True)
-    word_id = sa.Column(sa.Integer, sa.ForeignKey("word.id", ondelete='CASCADE'), nullable=False)
-    word = db.relationship("Word", backref='word', lazy=True)
-    status_id = sa.Column(sa.Integer, sa.ForeignKey("status.id", ondelete='CASCADE'), nullable=False)
-    status = db.relationship("Status", backref='status', lazy=True)
-    update_time = sa.Column(sa.DateTime, default=datetime.utcnow)
+class Association(db.Model):
+    # __tablename__ = 'association'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    word_id = db.Column(db.Integer, db.ForeignKey('word.id'), primary_key=True)
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    status = db.relationship('Status', backref=db.backref('associations', lazy='dynamic'))
+
+# class User_Has_Word(db.Model):
+#     id = sa.Column(sa.BigInteger, primary_key=True)
+#     user_id = sa.Column(sa.Integer, sa.ForeignKey("user.id", ondelete='CASCADE'), nullable=False)
+#     word_id = sa.Column(sa.Integer, sa.ForeignKey("word.id", ondelete='CASCADE'), nullable=False)
+#     status_id = sa.Column(sa.Integer, sa.ForeignKey("status.id", ondelete='CASCADE'), nullable=False)
+#     status = db.relationship("Status", backref='status', lazy=True)
+#     update_time = sa.Column(sa.DateTime, default=datetime.utcnow)
