c695049cbbe398e3fb2614a0e96998defd04ef44
Garrosh Magrathean <garroshm@student.21-school.ru>
2023-04-27 23:34:16 +0300

    start refactoring. logical options done

--- english_api/api/profile/words.py
+++ english_api/api/profile/words.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import request
+from flask import request, abort, Response, jsonify, make_response
 from sqlalchemy import func
 
 from english_api import db
@@ -33,40 +33,26 @@ def get_words():
     return create_res_obj(data=res), 200
 
 
-# @api.post("/word/")
-# def get_word():
-#     if not auth_check(request):
-#         return create_res_obj(status="FAILURE", description="Not found auth_token in headers", status_code=5), 403
-#     word_id = int(request.form.get("word_id"))
-#     status_id = int(request.form.get("status"))
-#     if not word_id or not status_id:
-#         return create_res_obj(status="FAILURE", description="No such param in body of request", status_code=3), 400
-#     if status_id == 1:
-#         pass
-#         # TODO надо поменять статус с НЕ ОТКРЫВАЛ на ОТКРЫЛ (1 -> 2)
-#     if status_id == 2:
-#         pass
-#         # TODO если слово было открыто то ждем пока пользователь ответит, знает он слово или нет. Значит отправить надо все данные для слова
-#     return create_res_obj(data={"target": request.form.get("word_id")}), 200
+def check_request(req):
+    if not auth_check(req):
+        abort(make_response(jsonify(**create_res_obj(status="FAILURE", description="Not found auth_token in headers",
+                                                     status_code=5)), 403))
+    status = req.form.get("status")
+    user_id = request.headers.get("auth_token")
+    if not status:
+        abort(make_response(jsonify(**create_res_obj(status="FAILURE", description="No such status of word",
+                                                     status_code=3)), 400))
+    word_id = req.form.get("word_id")
+    if not word_id:
+        abort(make_response(jsonify(**create_res_obj(status="FAILURE", description="No such word_id of word",
+                                                     status_code=3)), 400))
+    return word_id, user_id, status
 
 
 @api.post("/new_word")
 def get_new_word():
-    if not auth_check(request):
-        return create_res_obj(status="FAILURE", description="Not found auth_token in headers", status_code=5), 403
-    user_id = request.headers.get("auth_token")
-    status = request.form.get("status")
-    # TODO сделать валидацию на пришедший статус. Данный метод подразумевает что статус либо 1 либо 2
-    if not status:
-        return create_res_obj(status="FAILURE", description="No such status of word", status_code=3), 400
-    word_id = request.form.get("word_id")
-    if not word_id:
-        return create_res_obj(status="FAILURE", description="No such word_id of word", status_code=3), 400
+    word_id, user_id, status = check_request(request)
     row = UserWordStatus.query.filter_by(word_id=word_id, status_id=status, user_id=user_id).first()
-    if row.status_id == 1:
-        new_row = UserWordStatus(word_id=word_id, user_id=user_id, status_id=2)
-        db.session.add(new_row)
-        db.session.commit()
     word = Word.query.filter_by(id=row.word_id).first()
     word_schema = WordSchema()
     result = word_schema.dump(word)
@@ -75,15 +61,7 @@ def get_new_word():
 
 @api.post("/know_this_word")
 def know_this_word():
-    if not auth_check(request):
-        return create_res_obj(status="FAILURE", description="Not found auth_token in headers", status_code=5), 403
-    user_id = request.headers.get("auth_token")
-    status = request.form.get("status")
-    if not status:
-        return create_res_obj(status="FAILURE", description="No such status of word", status_code=3), 400
-    word_id = request.form.get("word_id")
-    if not word_id:
-        return create_res_obj(status="FAILURE", description="No such word_id of word", status_code=3), 400
+    word_id, user_id, status = check_request(request)
     new_row = UserWordStatus(word_id=word_id, user_id=user_id, status_id=8)
     db.session.add(new_row)
     db.session.commit()
@@ -92,19 +70,8 @@ def know_this_word():
 
 @api.post("/option_1")
 def option_1():
-    if not auth_check(request):
-        return create_res_obj(status="FAILURE", description="Not found auth_token in headers", status_code=5), 403
-    user_id = request.headers.get("auth_token")
-    status = request.form.get("status")
-    if not status:
-        return create_res_obj(status="FAILURE", description="No such status of word", status_code=3), 400
-    word_id = request.form.get("word_id")
-    if not word_id:
-        return create_res_obj(status="FAILURE", description="No such word_id of word", status_code=3), 400
+    word_id, user_id, status = check_request(request)
     this_word = Word.query.filter_by(id=word_id).first()
-    row = UserWordStatus(word_id=word_id, user_id=user_id, status_id=3)
-    db.session.add(row)
-    db.session.commit()
     user = User.query.filter_by(id=user_id).first()
     all_words = Word.query.filter_by(group_id=user.level).all()
     random_choice = [this_word.word_ru]
@@ -120,3 +87,68 @@ def option_1():
     }
     return create_res_obj(data=response, status_code=0), 200
 
+
+@api.post("/option_2")
+def option_2():
+    word_id, user_id, status = check_request(request)
+    this_word = Word.query.filter_by(id=word_id).first()
+    user = User.query.filter_by(id=user_id).first()
+    all_words = Word.query.filter_by(group_id=user.level).all()
+    random_choice = [this_word.word_ru]
+    while len(random_choice) < 4:
+        random_option = random.choice(all_words)
+        if random_option not in random_choice:
+            random_choice.append(random_option.word_ru)
+    response = {
+        "options": random_choice,
+        "answer": this_word.word_ru,
+        "audio": this_word.audio_path
+    }
+    return create_res_obj(data=response, status_code=0), 200
+
+
+@api.post("/option_3")
+def option_3():
+    word_id, user_id, status = check_request(request)
+    this_word = Word.query.filter_by(id=word_id).first()
+    user = User.query.filter_by(id=user_id).first()
+    all_words = Word.query.filter_by(group_id=user.level).all()
+    random_choice = [this_word.word_en]
+    while len(random_choice) < 4:
+        random_option = random.choice(all_words)
+        if random_option not in random_choice:
+            random_choice.append(random_option.word_en)
+    response = {
+        "options": random_choice,
+        "answer": this_word.word_en,
+        "image": this_word.image_path
+    }
+    return create_res_obj(data=response, status_code=0), 200
+
+
+@api.post("/option_4")
+def option_4():
+    word_id, user_id, status = check_request(request)
+    this_word = Word.query.filter_by(id=word_id).first()
+    user = User.query.filter_by(id=user_id).first()
+    all_words = Word.query.filter_by(group_id=user.level).all()
+    random_choice = [this_word.word_en]
+    while len(random_choice) < 4:
+        random_option = random.choice(all_words)
+        if random_option not in random_choice:
+            random_choice.append(random_option.word_en)
+    response = {
+        "options": random_choice,
+        "answer": this_word.word_en,
+        "word": this_word.word_ru
+    }
+    return create_res_obj(data=response, status_code=0), 200
+
+
+@api.post("/success_answer")
+def success_answer():
+    word_id, user_id, status = check_request(request)
+    row = UserWordStatus(user_id=user_id, word_id=word_id, status_id=int(status) + 1)
+    db.session.add(row)
+    db.session.commit()
+    return create_res_obj(description="Successful update status"), 200
